2c548ea851b26df3f7e8b141e6a9fd74dd0ea45e
jose04912 <josewilliams995@gmail.com>
2025-11-25 19:11:47 -0500

    Delete -Web-de-Freelance-main directory

--- -Web-de-Freelance-main/backend/ACRA/models/Servicio.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const ServicioSchema = new Schema({
-  nombreServicio: { type: String, required: true },
-  precioBase: { type: Number, required: true, min: 0 },
-  descripcion: { type: String }
-});
-
-module.exports = model('Servicio', ServicioSchema);
--- -Web-de-Freelance-main/backend/ACRA/router/cotizaciones.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const controller = require('../controllers/cotizacionesController');
-
-router.post('/', controller.createCotizacion);
-router.get('/', controller.listCotizaciones);
-router.put('/:id', controller.updateCotizacion);
-router.delete('/:id', controller.deleteCotizacion);
-
-module.exports = router;
